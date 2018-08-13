d1ecf92983c0d3d48a8c0a4f8f8bb99333bdc867
justin <bond.justink@gmail.com>
2018-08-13 12:40:36 -0700

    removed work, added myself

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'asap_s' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'asap_s' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'asap_s' ), 'asap_s', '<a href="https://530medialab.com" rel="designer">530MEDIALAB</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'asap_s' ), 'asap_s', '<a href="https://justin-bond.com" rel="designer">Justin Bond</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
